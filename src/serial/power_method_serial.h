3c88eaafff620a6e8e33f8009317f249cc6e8f43
Oliver <oliver.lagerqvist@outlook.com>
2025-04-04 20:24:32 +0200

    added comments

--- src/serial/power_method_serial.h
+++ src/serial/power_method_serial.h
@@ -5,15 +5,15 @@
 #include "../../include/vector.h"
 #include <stdbool.h>
 
-// Function to run the power method
+// Function for dense matrices.
 double serial_dense_power_method(denseMatrix*);
 void serial_dense_matvec_mult(denseMatrix*, Vector*);
 double serial_dense_approximate_eigenvalue(denseMatrix*, Vector*);
-
+// Function for sparse matrices.
 double serial_sparse_power_method(sparseMatrix*);
 void serial_sparse_matvec_mult(sparseMatrix*, Vector*);
 double serial_sparse_approximate_eigenvalue(sparseMatrix*, Vector*);
-
+// Common functions.
 void serial_normalize_vector(Vector*);
 bool serial_convergence(double, double, double);
 #endif 
