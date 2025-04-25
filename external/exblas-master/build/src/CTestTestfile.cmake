5f3853a5aac5fc73026e750256f75bae6c47934b
Oliver <oliver.lagerqvist@outlook.com>
2025-04-25 13:20:48 +0200

    added ExBLAS dict

--- external/exblas-master/build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/olive/skola/Reproducible-power-method-in-parallel-environments/external/exblas-master/src
-# Build directory: /mnt/c/Users/olive/skola/Reproducible-power-method-in-parallel-environments/external/exblas-master/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cpu")
