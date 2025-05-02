a4c504748edb7b2b5d9ab6a053faa9d750716236
Oliver <oliver.lagerqvist@outlook.com>
2025-05-02 21:56:36 +0200

    contiue with cuda dot

--- src/CUDA/cuda_fun.cpp
+++ src/CUDA/cuda_fun.cpp
@@ -4,39 +4,40 @@
 #include <stdlib.h>
 #include "cuda_fun.h"
 #include <stdio.h>
+
+#define WARP_COUNT 8
+#define WARP_SIZE 32
+#define BLOCK_SIZE (WARP_COUNT * WARP_SIZE)
+#define NUM_BLOCKS
+
 template <unsigned int blockSize>
 __device__ void warpReduce(volatile int *sdata, unsigned int tid) {
-if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
-if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
-if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
-if (blockSize >= 8) sdata[tid] += sdata[tid + 4];
-if (blockSize >= 4) sdata[tid] += sdata[tid + 2];
-if (blockSize >= 2) sdata[tid] += sdata[tid + 1];
+    if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+    if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+    if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+    if (blockSize >= 8) sdata[tid] += sdata[tid + 4];
+    if (blockSize >= 4) sdata[tid] += sdata[tid + 2];
+    if (blockSize >= 2) sdata[tid] += sdata[tid + 1];
 }
 
-/*
-int *g_idata - input array in global memory.
 
-int *g_outdata - output array in global memory.
-
-unsigned int n - total number of elements in the input array.
-
-*/
-template <unsigned int blockSize, typename Op>
-__global__ void reduce(double *g_idata, double *g_odata, unsigned int n, Op, op) {
-extern __shared__ double sdata[];
-unsigned int tid = threadIdx.x;
-unsigned int i = blockIdx.x*(blockSize*2) + tid;
-// Jump distance for strided loop
-unsigned int gridSize = blockSize*2*gridDim.x;
-sdata[tid] = 0;
-while (i < n) { sdata[tid] += op(g_idata[i], g_idata[i + blockSize]); i += gridSize; }
-__syncthreads();
-if (blockSize >= 512) { if (tid < 256) { sdata[tid] += sdata[tid + 256]; } __syncthreads(); }
-if (blockSize >= 256) { if (tid < 128) { sdata[tid] += sdata[tid + 128]; } __syncthreads(); }
-if (blockSize >= 128) { if (tid < 64) { sdata[tid] += sdata[tid + 64]; } __syncthreads(); }
-if (tid < 32) warpReduce(sdata, tid); 
-if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+template <unsigned int blockSize>
+__global__ void reduce(double *x_idata, double *y_idata, int *odata, unsigned int n) {
+    extern __shared__ int sdata[];
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*(blockSize*2) + tid;
+    unsigned int gridSize = blockSize*2*gridDim.x;
+    sdata[tid] = 0;
+    while (i < n) { 
+        sdata[tid] += x_idata[i] * y_idata[i] + x_idata[i+blockSize] * y_idata[i+blockSize];
+        i += gridSize; 
+    }
+    __syncthreads();
+    if (blockSize >= 512) { if (tid < 256) { sdata[tid] += sdata[tid + 256]; } __syncthreads(); }
+    if (blockSize >= 256) { if (tid < 128) { sdata[tid] += sdata[tid + 128]; } __syncthreads(); }
+    if (blockSize >= 128) { if (tid < 64) { sdata[tid] += sdata[tid + 64]; } __syncthreads(); }
+    if (tid < 32) warpReduce(sdata, tid);
+    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
 
 double cuda_dot_product(const Vector* x, const Vector* y){
@@ -79,3 +80,43 @@ double cuda_dot_product(const Vector* x, const Vector* y){
 
 }
 
+
+__global__ void DOT(const double* d_x, const double* d_y, const int incx, const int incy, const int offsetx, const int offsety, const int NbElements, double* sums){
+    
+    extern __shared__ double sdata[];
+
+    int local_id = threadIdx.x;
+    int global_id = blockIdx.x * blockDim.x + local_id;
+    int total_threads = blockDim.x * gridDim.x;
+
+    sdata[local_id] = 0.0;
+    for(int pos = global_id; pos < NbElements; pos += total_threads){
+        sdata[local_id] += d_x[pos] * d_y[pos];
+    }
+    __syncthreads();
+    
+}
+
+__global__ void DOTComplete(){
+
+
+}
+
+
+double cuda_dot(const Vector* x, const Vector* y, const int incx, const int incy, const int offsetx, const int offsety){
+    int size_x = x->size;
+    int size_y = y->size;
+    const double* d_x, d_y;
+
+    if(size_x != size_y){
+        printf("Error: Vectors must have the same size (x: %d, y: %d)\n", size_x, size_y);
+        return 0.0;
+    }
+    //Allocate CUDA memory
+    cudaMalloc(&d_x, sizeof(double) * size_x);
+    cudaMalloc(&d_y, sizeof(double) * size_y);
+    cudaMemcpy(d_x, x->data, sizeof(double) * size_x, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y->data, sizeof(double) * size_y, cudaMemcpyHostToDevice);
+
+    DOT<<<>>>
+}
